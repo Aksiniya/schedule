c8f63b5c12475b99cec6a2f3b93201acbb433c0b
Nadezda Agildina <a_strelina@inbox.ru>
2018-01-18 23:31:38 +0300

    add pods

---
+++ Расписание МГТУ/AppSettings.swift
@@ -0,0 +1,13 @@
+//
+//  AppSettings.swift
+//  Расписание
+//
+//  Created by macbook on 17.01.18.
+//  Copyright © 2018 macbook. All rights reserved.
+//
+
+import Cocoa
+
+class AppSettings: UITableViewController {
+
+}
